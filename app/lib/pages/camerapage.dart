b9743a667c21dbddbe13c540d47ed44a2fdc261f
SANKIT KUNDU <sankitkundu96@gmail.com>
2023-04-15 16:49:06 +0530

    added all the files

--- app/lib/pages/camerapage.dart
+++ app/lib/pages/camerapage.dart
@@ -10,6 +10,7 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  bool ispause = true;
   @override
   Widget build(BuildContext context) {
     final devicesize = MediaQuery.of(context).size;
@@ -31,7 +32,77 @@ class _CameraPageState extends State<CameraPage> {
               clipBehavior: Clip.hardEdge,
               child: PushedPage(cameras: widget.cameras),
             ),
-            Row(children: [Container(), Container(), Container(), Container()])
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(
+                width: devicesize.width * 0.2,
+                height: devicesize.width * 0.2,
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(255, 224, 180, 1),
+                    borderRadius: BorderRadius.circular(10)),
+                child:
+                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  Text('sets'),
+                  SizedBox(
+                    height: 4,
+                  )
+                ]),
+              ),
+              Container(
+                  width: devicesize.width * 0.2,
+                  height: devicesize.width * 0.2,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 224, 180, 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text('reps'),
+                        SizedBox(
+                          height: 4,
+                        )
+                      ])),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: devicesize.width * 0.2,
+                  height: devicesize.width * 0.2,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 94, 88, 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Container(
+                      width: devicesize.width * 0.07,
+                      height: devicesize.width * 0.07,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: devicesize.width * 0.2,
+                  height: devicesize.width * 0.2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: const Color.fromRGBO(71, 71, 71, 1),
+                      width: 2,
+                    ),
+                  ),
+                  child: Center(
+                      child: Icon(
+                    ispause ? Icons.play_arrow_rounded : Icons.pause,
+                    size: 28,
+                  )),
+                ),
+              )
+            ])
           ],
         ),
       ),
