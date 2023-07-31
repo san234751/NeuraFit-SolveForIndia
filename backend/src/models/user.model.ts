5e8d9561d96d4fbf770b528266fddcc85e3eac22
Manash Pratim Pathak <85903876+ManashPratimPathak@users.noreply.github.com>
2023-07-31 19:10:59 +0530

    Update user.model.ts

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,11 +1,6 @@
 import mongoose,{ Schema, InferSchemaType } from 'mongoose';
 
-const schema = new Schema({
-  id: {type: String, required: true, unique: true},
-  name: { type: String, required: true },
-  email: { type: String, required: true , unique: true, trim: true},
-  avatar: String
-});
+
 
 export type User = InferSchemaType<typeof schema>;
 export const UserModel = mongoose.model('User', schema);
