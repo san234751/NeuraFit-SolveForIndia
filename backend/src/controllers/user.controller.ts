fc09f6a2a5689fcbc8f10280974899a6d9083250
Manash Pratim Pathak <85903876+ManashPratimPathak@users.noreply.github.com>
2023-07-31 19:13:32 +0530

    Update user.controller.ts
    updated the user controller using typecript for better error handling and validation

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,3 +2,35 @@ import {Request , Response} from 'express'
 import {User, UserModel} from '@models/user.model'
 import {client} from '@configs/mongo'
 
+const connection = async ()=>{
+    const db = (await client.connect()).db("app");
+    const collection = db.collection("users");
+    return collection;
+}
+
+export default {
+    get : async (req: Request,res: Response) => {
+        try{
+            const users = await connection();
+            const usersDoc = await users.findOne({
+                id: req.params.id
+            });
+            
+            res.json({result: JSON.parse(JSON.stringify(usersDoc))}).status(200);
+        } catch(err) {
+            res.json({result: err}).status(500);
+        }
+    },
+    post : async (req: Request,res: Response) => {
+        
+        const user = new UserModel(req.body);
+        try{
+            const collection = await connection();
+            const result = await collection.insertOne(user);
+            res.json({result: result}).status(200);
+        } catch(err) {
+            res.json({result: err}).status(500);
+        }
+
+    }
+}
